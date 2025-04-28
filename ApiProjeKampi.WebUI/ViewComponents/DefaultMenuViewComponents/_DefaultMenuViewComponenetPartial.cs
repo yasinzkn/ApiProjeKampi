468f9acd8d20faea76be0f10afedb3aa4f294420
yasin <yasin@LAPTOP-8VQTI9O3>
2025-04-28 20:22:13 +0300

    .

---
+++ ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuViewComponenetPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuViewComponenetPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
