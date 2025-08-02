26f39e2e4eee15f10ac534d903be9fee0f58d112
yasin <yasin@LAPTOP-8VQTI9O3>
2025-08-02 15:45:52 +0300

    .

---
+++ ApiProjeKampi.WebUI/Controllers/AIController.cs
@@ -0,0 +1,72 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace ApiProjeKampi.WebUI.Controllers
+{
+    public class AIController : Controller
+    {
+        public IActionResult CreateRecipeWithOpenAI()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRecipeWithOpenAI(string prompt)
+        {
+            var apiKey = "";
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            var requestData = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new
+                    {
+                        role = "system", content = "Sen bir restoran için yemek önerileri yapan bir yapay zeka aracısın. Amacımız kullanıcı tarafından girilen malzemelere göre yemek tarifi önerisinde bulunmak."
+                    },
+                    new
+                    {
+                        role = "user", content = prompt
+                    }
+                },
+                temperature = 0.5
+            };
+
+            var response = await client.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestData);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<OpenAIResponse>();
+                var content = result.choices[0].message.content;
+                ViewBag.recipe = content;
+            }
+            else
+            {
+                ViewBag.recipe = "Bir hata oluştu: " + response.StatusCode;
+            }
+
+            return View();
+        }
+
+        public class OpenAIResponse
+        {
+            public List<Choice> choices { get; set; }
+        }
+
+        public class Choice
+        {
+            public Message message { get; set; }
+        }
+
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+        }
+
+    }
+}
